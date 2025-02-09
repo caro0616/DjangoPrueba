8bf1fa8f4438c43751f03329b232e96bdcca1141
caro0616 <lcobandof@eafit.edu.co>
2025-02-09 11:05:01 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+  #  return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, 'home.html', {'name':'Leidy obando'})
+
+def about(request):
+    return HttpResponse("Welcome to the About Page")
